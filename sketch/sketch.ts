8d0089edbb02bc6b7bbc680390344a2641667570
Alexander Weber <43758870+alexw-at@users.noreply.github.com>
2023-02-03 23:19:32 +0100

    add commands

--- sketch/sketch.ts
+++ sketch/sketch.ts
@@ -1,38 +1,51 @@
-let words: Word[] = []
+let words: Word[] = [];
 
-const pixelSize = window.innerHeight
-const terminal = new Terminal(pixelSize)
+const pixelSize = window.innerHeight;
+const terminal = new Terminal(pixelSize);
 
 function setup() {
-    textFont('monospace')
-    createCanvas(800, pixelSize)
-    words.push(new Word('hello', 40, 2))
-    words.push(new Word('world', 400, 2))
+    textFont('monospace');
+    createCanvas(800, pixelSize);
+}
+
+const commands = [
+    'kill --pid {randi}',
+    'close port {randi}',
+    'start process --id {randi}',
+    'update firewall',
+    'close connection {randi} --force',
+];
+
+function getCommand() {
+    return random(commands).replace(
+        '{randi}',
+        Math.floor(random(0, 999)).toString()
+    );
 }
 
 function draw() {
-    background(200)
+    background(255);
 
-    terminal.draw()
-    words.forEach((w) => w.update())
-    words.forEach((w) => w.draw())
-    words = words.filter((w) => w.posY < pixelSize)
+    terminal.draw();
+    words.forEach((w) => w.update());
+    words.forEach((w) => w.draw());
+    words = words.filter((w) => w.posY < pixelSize);
 
     if (words.length === 0) {
-        words.push(new Word('foo', 200, 3))
+        words.push(new Word(getCommand(), random(0, pixelSize), 2));
     }
 }
 
 function keyTyped() {
     if (key === 'Enter') {
-        terminal.send()
+        terminal.send();
     } else {
-        terminal.addKey()
+        terminal.addKey();
     }
 }
 
 function keyPressed() {
     if (keyCode === BACKSPACE) {
-        terminal.backspace()
+        terminal.backspace();
     }
 }
