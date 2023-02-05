bac25d3fd467c1a7c2712eb50831cad6fd74fb99
ein-bandit <ein-bandit@tuta.io>
2023-02-05 17:13:49 +0100

    added whoami special command

--- sketch/terminal.ts
+++ sketch/terminal.ts
@@ -9,6 +9,7 @@ const SPECIAL_COMMANDS: { [key: string]: string } = {
     'hard mode': 'HARD MODE activated',
     debug: 'DEBUG MODE enabled permanently in this browser',
     exit: 'we need you to defend the system - no running away!',
+    whoami: 'master of the universe, defender of the systems, best ethical hacker in town',
 };
 
 class Terminal {
