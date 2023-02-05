9d23f10e0679593b1c7d5692bd5647e434a99fb7
ein-bandit <6816384+ein-bandit@users.noreply.github.com>
2023-02-05 16:22:30 +0000

    Deploying to gh-pages from @ terminal-takedown/terminal-takedown@0990862262b6541810b8331bc5e45ebb17d77e29 🚀

--- build/sketch.js
+++ build/sketch.js
@@ -88,11 +88,13 @@ function draw() {
     matrixParticles.forEach((p) => p.update());
     matrixParticles = matrixParticles.filter((p) => p.chars.length < 25);
     if (gameState === 'boot') {
-        image(img, innerWidth / 2 - 512 / 2, innerHeight / 2 - 512 / 2 - 128, 512, 512);
+        const factor = 0.5;
+        const y = windowHeight / 2 - (windowHeight * factor) / 2 - windowHeight * 0.1;
+        image(img, windowWidth / 2 - (windowHeight * factor) / 2, y, windowHeight * factor, windowHeight * factor);
         const name = 'TERMINAL TAKEDOWN';
         textSize(32);
         fill('green');
-        text(name, windowWidth / 2 - textWidth(name) / 2, innerHeight / 2 + 192);
+        text(name, windowWidth / 2 - textWidth(name) / 2, y + windowHeight * factor + 64);
     }
     else if (gameState === 'initial') {
         fill(200);
