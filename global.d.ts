2af7bb209f9630755b9f719d753e8e5ada13fc66
ein-bandit <ein-bandit@tuta.io>
2023-02-07 01:43:33 +0100

    fixed ts things

--- global.d.ts
+++ global.d.ts
@@ -1,5 +1,16 @@
 // This file will add both p5 instanced and global intellisence
-import * as p5Global from "p5/global";
-import module = require("p5");
+import * as p5Global from 'p5/global';
+import { SoundFile } from 'p5';
+import module = require('p5');
 export = module;
 export as namespace p5;
+
+declare global {
+    function loadSound(
+        path: string | any[],
+        successCallback?: (...args: any[]) => any,
+        errorCallback?: (...args: any[]) => any,
+        whileLoading?: (...args: any[]) => any
+    ): p5.SoundFile;
+    function soundFormats(f1: string, f2: string): void;
+}
